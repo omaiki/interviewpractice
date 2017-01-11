540337d84438cc63338af57b351dfe2943762fdc
Oshoke Maiki <oshokemaiki@gmail.com>
2017-01-11 04:17:15 -0800

    Setup data structures file: add problems for solving

---
+++ data_structures.rb
@@ -0,0 +1,60 @@
+# Implement an algorithm to determine if a string has all unique characters. What if you
+# can not use additional data structures?
+
+
+#-------------------------------------------------------
+
+# Write code to reverse a C-Style String. (C-String means that “abcd” is represented as five characters, including the null character.)
+
+
+
+
+# -------------------------------------------------------
+
+# Design an algorithm and write code to remove the duplicate characters in a string
+# without using any additional bu#er. NOTE: One or two additional variables are !ne.
+# An extra copy of the array is not.
+# FOLLOW UP
+# Write the test cases for this method.
+
+
+
+# -------------------------------------------------------
+
+# Write a method to decide if two strings are anagrams or not
+
+
+
+
+# -------------------------------------------------------
+
+# Write a method to replace all spaces in a string with ‘%20’
+
+
+
+
+# -------------------------------------------------------
+
+# Given an image represented by an NxN matrix, where each pixel in the image is 4
+# bytes, write a method to rotate the image by 90 degrees. Can you do this in place?
+
+
+
+
+# -------------------------------------------------------
+
+# Write an algorithm such that if an element in an MxN matrix is 0, its entire row and
+# column is set to 0.
+
+
+
+
+# -------------------------------------------------------
+
+# Assume you have a method isSubstring which checks if one word is a substring of
+# another. Given two strings, s1 and s2, write code to check if s2 is a rotation of s1 using
+# only one call to isSubstring (i.e., “waterbottle” is a rotation of “erbottlewat”).
+
+
+
+# -------------------------------------------------------
