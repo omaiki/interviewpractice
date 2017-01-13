01f13068b4be4c475a742c8607310ea3e8993354
Oshoke Maiki <oshokemaiki@gmail.com>
2017-01-13 01:42:55 -0800

    Complete algorithm 1.1

--- arrays_and_strings.rb
+++ arrays_and_strings.rb
@@ -1,6 +1,16 @@
 # Implement an algorithm to determine if a string has all unique characters. What if you
 # can not use additional data structures?
 
+def unique_chars?(string)
+  string_array = string.chars
+  unique_array = string_array.uniq
+  if string_array == unique_array
+    return false
+  else
+    return true
+  end
+end
+
 
 
 
