e557d77a027f79647aa3191b12d91830e02f24a1
Oshoke Maiki <oshokemaiki@gmail.com>
2017-01-16 05:32:39 -0800

    Pause on 1.8 and 1.9, write Rspec tests

--- ch1_arrays_and_strings/1.8.rb
+++ ch1_arrays_and_strings/1.8.rb
@@ -1,7 +1,25 @@
 # Write an algorithm such that if an element in an MxN matrix is 0, its entire row and
 # column is set to 0.
 
+# require 'matrix'
 
+# def zero_matrix(matrix)
+#   matrix.length.times do |row|
+#     if matrix[row].include?(0)
+#      matrix[row].each do |element|
+#         element = 0
+#         p element
+#       end
+#     end
+#   end
+
+# end
 
 
 # -------------------------------------------------------
+
+
+# p zero_matrix([[1, 0, 3], [4, 5, 6], [7, 8, 9]])
+# [[1, 0, 3], [4, 5, 6], [7, 8, 9]]
+
+# [[0, 0, 0], [4, 0, 6], [7, 0, 9]]
