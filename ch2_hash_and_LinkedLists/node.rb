a40e59f571fba6b2cf65078a1104f142044d5f9b
Oshoke Maiki <oshokemaiki@gmail.com>
2017-01-22 20:47:28 -0800

    Formatting, set up node class for linked_list practice

---
+++ ch2_hash_and_LinkedLists/node.rb
@@ -0,0 +1,27 @@
+class Node
+
+  attr_accessor :value, :next_node
+
+  def initialize(value, next_node)
+    @value = value
+    @next_node = next_node
+  end
+
+
+  # print a linked_list
+
+
+
+  # Reverse a linked list given its head
+
+
+
+
+
+  # Implement a stack using a linked list
+
+
+
+
+
+end
